6dd7b47977815d2c29bf85c347c905a09ef9256a
Andrianna <andrianna.odell@gmail.com>
2022-09-09 12:07:21 -0400

    commit tree 335e49c0ec9b3d4365cfc72983e7f58f623a3977

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_error
+  before_action :authorize
+
+  private
+
+  # determine if the current user logging in matches the information
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+		return render json: { error: "Not authorized" }, status: :unauthorized unless @current_user
+	end
+
+  def render_unprocessable_entity_error(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
